5cee73d08812b3e071cd14b6c69eb2ee710b7f27
Adriaan Beiertz <adriaanbd@gmail.com>
2020-10-14 01:34:03 -0500

    added method to deallocate line and refactored Batch

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Set
 from datetime import date
 
 
@@ -15,12 +15,28 @@ class Batch:
         self, ref: str, sku: str, qty: int, eta: Optional[date] = None):
         self.ref = ref
         self.sku = sku
-        self.qty = qty
         self.eta = eta
-
-    def _can_allocate(self, line: OrderLine):
-        return self.qty >= line.qty and self.sku == line.sku
+        self.qty = qty
+        self._allocations: Set[OrderLine] = set()
 
     def allocate(self, line: OrderLine):
         if self._can_allocate(line):
-            self.qty -= line.qty
+            self._allocations.add(line)
+    
+    def deallocate(self, line: OrderLine):
+        allocated = line in self._allocations
+        if allocated:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_qty(self):
+        qty = sum(line.qty for line in self._allocations)
+        return qty
+
+    @property
+    def available_qty(self):
+        qty = self.qty - self.allocated_qty
+        return qty
+
+    def _can_allocate(self, line: OrderLine):
+        return self.qty >= line.qty and self.sku == line.sku
--- test_model.py
+++ test_model.py
@@ -17,20 +17,25 @@ def make_batch_n_line(sku, batch_qty, line_qty):
 def test_does_allocate_if_available_greater_than_required():
     batch, line = make_batch_n_line(SKU, QUANTITY, SMALLER)
     batch.allocate(line)
-    assert batch.qty == QUANTITY - SMALLER
+    assert batch.available_qty == QUANTITY - SMALLER
 
 def test_doesnt_allocate_if_available_smaller_than_required():
     batch, line = make_batch_n_line(SKU, QUANTITY, GREATER)
     batch.allocate(line)
-    assert batch.qty == QUANTITY
+    assert batch.available_qty == QUANTITY
 
 def test_does_allocate_if_available_equal_to_required():
     batch, line = make_batch_n_line(SKU, QUANTITY, QUANTITY)
     batch.allocate(line)
-    assert batch.qty == 0
+    assert batch.available_qty == 0
 
 def test_doesnt_allocate_if_skus_do_not_match():
     batch = Batch(BATCH_REF, SKU, QUANTITY)
     line = OrderLine(ORDER_REF, 'SANE_LAMP', SMALLER)
     batch.allocate(line)
-    assert batch.qty == QUANTITY
+    assert batch.available_qty == QUANTITY
+
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_n_line(SKU, QUANTITY, SMALLER)
+    batch.deallocate(unallocated_line)
+    assert batch.available_qty == QUANTITY
