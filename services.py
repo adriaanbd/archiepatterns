7fb4b635d4ec8fdd03487ec3c2cf2709b2a38e98
Adriaan Beiertz <adriaanbd@gmail.com>
2020-12-16 00:01:00 -0500

    added type hints and docstrings for services

--- services.py
+++ services.py
@@ -1,18 +1,28 @@
-import model
+from model import allocate as domain_allocate
+from model import OrderLine, Batch
+from repository import AbstractRepository
+from typing import List
 
 
 class InvalidSKU(Exception):
 	pass
 
 
-def is_valid_sku(line, batches):
+def is_valid_sku(line: OrderLine, batches: List[Batch]) -> bool:
+	"""
+	Validates an OrderLine's SKU against a list of Batches' SKU.
+	"""
 	skus = {b.sku for b in batches}
 	return line.sku in skus
 
-def allocate(line, repo, session):
+def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
+	"""
+	Obtains a list of Batches from data layer, validates OrderLine,
+	calls the allocate domain service, and commits to database.
+	"""
 	batches = repo.list()
 	if not is_valid_sku(line, batches):
 		raise InvalidSKU(f'Invalid SKU: {line.sku}')
-	ref = model.allocate(line, batches)
+	ref = domain_allocate(line, batches)
 	session.commit()
 	return ref
