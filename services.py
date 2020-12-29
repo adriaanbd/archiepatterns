d1b3f29cb0b850a81a553db0403c1d54e80c2c6b
Adriaan Beiertz <adriaanbd@gmail.com>
2020-12-29 01:09:25 -0500

    updated services with explicit imports

--- services.py
+++ services.py
@@ -1,5 +1,4 @@
-from model import allocate as domain_allocate
-from model import OrderLine, Batch
+import model
 from repository import AbstractRepository
 from typing import List
 
@@ -11,14 +10,14 @@ class UnallocatedSKU(Exception):
 	pass
 
 
-def is_valid_sku(line: OrderLine, batches: List[Batch]) -> bool:
+def is_valid_sku(line: model.OrderLine, batches: List[model.Batch]) -> bool:
 	"""
 	Validates an OrderLine's SKU against a list of Batches' SKU.
 	"""
 	skus = {b.sku for b in batches}
 	return line.sku in skus
 
-def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
+def allocate(line: model.OrderLine, repo: AbstractRepository, session) -> str:
 	"""
 	Obtains a list of Batches from data layer, validates OrderLine,
 	calls the allocate domain service, and commits to database.
@@ -26,19 +25,19 @@ def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
 	batches = repo.list()
 	if not is_valid_sku(line, batches):
 		raise InvalidSKU(f'Invalid SKU: {line.sku}')
-	ref = domain_allocate(line, batches)
+	ref = model.allocate(line, batches)
 	session.commit()
 	return ref
 
-def add_batch(batch: Batch, repo: AbstractRepository, session):
+def add_batch(batch: model.Batch, repo: AbstractRepository, session):
 	repo.add(batch)
 	session.commit()
 
 def is_allocated_sku(line, batch):
 	return batch.has_been_allocated(line)
 
-def deallocate(line: OrderLine, ref: str, repo: AbstractRepository, session):
-	batch: Batch = repo.get(ref)
+def deallocate(line: model.OrderLine, ref: str, repo: AbstractRepository, session):
+	batch: model.Batch = repo.get(ref)
 	if not is_allocated_sku(line, batch):
 		raise UnallocatedSKU(f'Unallocated SKU: {line.sku}')
 	batch.deallocate(line)
