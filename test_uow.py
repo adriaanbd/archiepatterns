ce240361874fd94444652c07ef1da6d0a90c2de5
Adriaan Beiertz <adriaanbd@gmail.com>
2021-01-14 22:48:19 -0500

    implemented unit of work

--- test_uow.py
+++ test_uow.py
@@ -12,15 +12,29 @@ def insert_batch(session, ref, sku, qty, eta):
         dict(ref=ref, sku=sku, qty=qty, eta=eta)
     )
 
+def get_allocated_batch_ref(session, orderid, sku):
+    [[orderlineid]] = session.execute(
+        'SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku',
+        dict(orderid=orderid, sku=sku)
+    )
+    [[batchref]] = session.execute(
+        'SELECT b.ref FROM allocations JOIN batches AS b ON batch_id = b.id'
+        ' WHERE orderline_id=:orderlineid',
+        dict(orderlineid=orderlineid)
+    )
+    return batchref
+
 def test_uow_can_retrieve_a_batch_and_allocate_to_it(session_factory):
     session = session_factory()
     insert_batch(session, BATCH1, REAL_SKU, MORE, None)
     session.commit()
 
-    uow = unit_of_work.SQLAlchemyUnitOfWork(session)
+    uow = unit_of_work.SQLAlchemyUnitOfWork(session_factory)
     with uow:
         batch = uow.batches.get(reference=BATCH1)
         line = model.OrderLine(ORDER1, REAL_SKU, LESS)
         batch.allocate(line)
         uow.commit()
-        assert uow.committed
+
+    batchref = get_allocated_batch_ref(session, ORDER1, REAL_SKU)
+    assert batchref == BATCH1
--- unit_of_work.py
+++ unit_of_work.py
@@ -1,9 +1,26 @@
+import repository
+import config
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DEFAULT_SESSION_FACTORY = sessionmaker(bind=create_engine(
+    config.get_postgres_uri(),
+))
 class SQLAlchemyUnitOfWork:
-    def __init__(self, session):
-        self.session = session
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
+        self.session_factory = session_factory
 
     def __enter__(self):
-        pass
+        self.session = self.session_factory()
+        self.batches = repository.SQLAlchemyRepository(self.session)
+        # return super().__enter__()  # needs the ABC
 
-    def __exit__(self):
-        pass
+    def __exit__(self, *args):
+        self.rollback()
+        self.session.close()
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
